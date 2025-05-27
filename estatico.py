b6ce231435bfad00332099c79d08199c2bce064a
Leonardo172775 <leonardo.cruz26@escola.pr.gov.br>
2025-05-27 08:30:42 -0300

    Create estatico.py

---
+++ estatico.py
@@ -0,0 +1,15 @@
+class Calculadora:
+    @staticmethod
+    def somar(a, b):
+        return a + b
+
+    @staticmethod
+    def subtrair(a, b):
+        return a - b
+
+# Testando os métodos estáticos
+resultado_soma = Calculadora.somar(10, 5)
+resultado_subtracao = Calculadora.subtrair(10, 5)
+
+print(f"Soma: {resultado_soma}")
+print(f"Subtração: {resultado_subtracao}")
