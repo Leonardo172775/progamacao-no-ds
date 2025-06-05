bf41587e08682444d822dd523870a0b0189d5d29
Leonardo172775 <leonardo.cruz26@escola.pr.gov.br>
2025-06-05 11:48:39 -0300

    Create  intervalo.py

---
+++ intervalo.py
@@ -0,0 +1,6 @@
+# Solicita ao usuário que insira um número inteiro
+limite = int(input("Digite um número inteiro: "))
+
+# Gera e exibe a sequência de números de 0 até o número fornecido
+for numero in range(limite + 1):
+    print(numero)
